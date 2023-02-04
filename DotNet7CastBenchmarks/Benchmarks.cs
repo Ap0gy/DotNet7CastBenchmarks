f4c8206cc2ed019462e8ec3ca6002a0faf92c339
Alex Tennyson-Davies <alex.tennysondavies@gmail.com>
2023-02-04 16:46:31 +0000

    Added benchmarking

---
+++ DotNet7CastBenchmarks/Benchmarks.cs
@@ -0,0 +1,32 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace DotNet7CastBenchmarks;
+
+
+[MemoryDiagnoser(false)]
+public class Benchmarks
+{
+    [Benchmark]
+    public Person SafeCast()
+    {
+        Person? safeCastedPersonObject = StaticObjects.PersonObject as Person;
+        return safeCastedPersonObject!;
+    }
+
+    [Benchmark]
+    public Person MatchCast()
+    {
+        if (StaticObjects.PersonObject is Person matchCastedpersonObject)
+        {
+            return matchCastedpersonObject;
+        }
+        return null!;
+    }
+
+    [Benchmark]
+    public Person HardCast()
+    {
+        Person personHardCast = (Person)StaticObjects.PersonObject;
+        return personHardCast;
+    }
+ }
--- DotNet7CastBenchmarks/Program.cs
+++ DotNet7CastBenchmarks/Program.cs
@@ -1,11 +1,4 @@
-﻿using DotNet7CastBenchmarks;
+﻿using BenchmarkDotNet.Running;
+using DotNet7CastBenchmarks;
 
-//Person personObject = (Person)StaticObjects.PersonObject;
-//Person? personObject = StaticObjects.PersonObject as Person;
-
-if(StaticObjects.PersonObject is not Person personObject)
-{
-    return;
-}
-
-Console.WriteLine(personObject.Name);
+BenchmarkRunner.Run<Benchmarks>();
